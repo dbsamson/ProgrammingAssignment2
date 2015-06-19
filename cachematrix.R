85bb7cdbc3a0819ddd4cb6078bd1676acfcb43da
David Samson <davidsamson.efrat@gmail.com>
2015-06-19 11:18:29 +0300

    First set of changes to adapt to matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #declare an empty matrix
+  cachedCopy <- matrix(NA,2,2)
+  
+  set <- function(y) {
+  ## clears the cached copy and passes the result to the calling function. 
+    cachedCopy <<- matrix(NA,2,2)
+    x<<- y
+  }
+  get <- function() x
+  setinverse <- function(inverse) { cachedCopy <<- inverse }
+  getinverse <- function() { cachedCopy }
+  list(set= set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+  
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns a matrix that is the inverse of 'x'.
+## We let R solve for the result inverse matrix, by providing the identity 
+## matrix.   
+  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <-  solve(data,diag(length(data[1,])))
+  x$setinverse(m)
+  m
 }
